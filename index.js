6059a8c07f07d6d1b5e2e4d99ec89aa2bbf0eca0
James Nzuki <nzukijames200@gmail.com>
2026-04-16 00:54:24 +0300

    implemented login tracker with closure and arrow functions

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
 
+    return (passwordAttempt) => {
+        attemptCount++;
 
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            if (attemptCount === 3) {
+                return `Attempt ${attemptCount}: Login failed`;
+            }
+            return `Attempt ${attemptCount}: Login failed`;
+        }
+    };
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = { createLoginTracker };
+}
